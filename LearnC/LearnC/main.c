c0c182404b652d48b75e9bc47d8d4214af9531f7
Amit Jangirh <amitjangirh@gmail.com>
2021-05-15 19:11:58 +0530

    Added C project

---
+++ LearnC/LearnC/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  LearnC
+//
+//  Created by Amit Jangirh on 15/05/21.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void performSomething() {
+    
+}
+
+int main(int argc, const char * argv[]) {
+    performSomething();
+    return 0;
+}
