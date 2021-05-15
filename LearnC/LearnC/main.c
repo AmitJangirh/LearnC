dfe16daa43f962be8d162461cc50cc35c64d7c0c
Amit Jangirh <amitjangirh@gmail.com>
2021-05-15 19:20:02 +0530

    Add create array function using malloc

--- LearnC/LearnC/main.c
+++ LearnC/LearnC/main.c
@@ -9,11 +9,21 @@
 #include <stdlib.h>
 
 
-void performSomething() {
-    
+void createAndPrintArray() {
+    // ask to create size of array
+    int n;
+    printf("Enter size of array\n");
+    scanf("%d", &n);
+    int *array = (int*) malloc(sizeof(int));
+    for(int i=0; i<n; i++) {
+        array[i] = i+1;
+    }
+    for(int i=0; i<n; i++) {
+        printf("%d\n", array[i]);
+    }
 }
 
 int main(int argc, const char * argv[]) {
-    performSomething();
+    createAndPrintArray();
     return 0;
 }
