ed2920b597b810469a52f09dd17cd2829d9a0b60
Amit Jangirh <amitjangirh@gmail.com>
2021-05-15 19:42:06 +0530

    Updating util functions

--- LearnC/LearnC/main.c
+++ LearnC/LearnC/main.c
@@ -8,22 +8,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Created to understand Malloc and calloc initialing 
+ */
 
-void createAndPrintArray() {
-    // ask to create size of array
-    int n;
-    printf("Enter size of array\n");
-    scanf("%d", &n);
-    int *array = (int*) malloc(sizeof(int));
-    for(int i=0; i<n; i++) {
-        array[i] = i+1;
-    }
-    for(int i=0; i<n; i++) {
+void printArray(int array[]) {
+    printf("***** Printing Array array \n");
+    int size = sizeof array / sizeof array[0];
+    for(int i=0; i<size; i++) {
         printf("%d\n", array[i]);
     }
 }
 
+void getSizeOfArray(int *n) {
+    // ask to create size of array
+    printf("Enter size of array\n");
+    scanf("%d", n);
+}
+
+void initialiseArray(int array[]) {
+    printf("***** initialising \n");
+    int size = sizeof array / sizeof array[0];
+    for(int i=0; i<size; i++) {
+        array[i] = i+1;
+    }
+}
+
+void createAndPrintArrayUsingMalloc() {
+    printf("***** MALLOC \n");
+    int n;
+    getSizeOfArray(&n);
+    int *array = (int*) malloc(n*sizeof(int));   // Dynamic allocation of array
+    printArray(array);                          // Before initialisation, prints garbage value
+    initialiseArray(array);
+    printArray(array);
+}
+
+void createAndPrintArrayUsingCalloc() {
+    printf("***** CALLOC \n");
+    int n;
+    getSizeOfArray(&n);
+    int *array = (int*) calloc(n, sizeof(int)); // Dynamic allocation of array
+    printArray(array);                          // Before initialisation, prints 0
+    initialiseArray(array);
+    printArray(array);
+}
+
 int main(int argc, const char * argv[]) {
-    createAndPrintArray();
+    createAndPrintArrayUsingMalloc();
+    createAndPrintArrayUsingCalloc();
     return 0;
 }
